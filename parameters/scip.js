5ceac5b891e4146c681d334716832df64c6fef7a
Ingmar Vierhaus <vierhaus@zib.de>
2017-01-08 19:16:49 +0100

    Added sdscip to scip params binaryTypes

--- parameters/scip.js
+++ parameters/scip.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
     id :"scip",
     label:  "SCIP Parameter",
-    binaryTypes: ['scip'],
+    binaryTypes: ['scip', 'sdscip'],
 
 
     getLabel : function getLabel() {
