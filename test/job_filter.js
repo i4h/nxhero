3fdf1a86932c9f0c2fa429d6fdf395c96552744d
Ingmar Vierhaus <vierhaus@zib.de>
2017-01-10 17:33:58 +0100

    Update job_filter and jobFilter view

--- test/job_filter.js
+++ test/job_filter.js
@@ -4,21 +4,23 @@ chai.use(assertArrays);
 var expect  = require("chai").expect;
 var sinon = require('sinon');
 var debug       = require('debug')('nxhero');
+var extend = require("extend");
 
 require("./__shared");
 
 JobFilter = require('../lib/job_filter');
 
 describe("job_filter module", function() {
+
     describe("makeWhereClause", function() {
-        it("makes a normal where clause", sinon.test(function (done) {
+        it("make a normal where clause", sinon.test(function (done) {
             var clause = JobFilter.makeWhereClause("id", 1, null);
             expect(clause).to.be.an.object;
             expect(clause.id).to.be.equal(1);
             done();
         }));
 
-        it("makes a related where clause", sinon.test(function (done) {
+        it("make a related where clause", sinon.test(function (done) {
             var clause = JobFilter.makeWhereClause("problem.id", 1, null);
             expect(clause).to.be.an.object;
             expect(clause.problem).to.be.an.object;
@@ -26,7 +28,7 @@ describe("job_filter module", function() {
             done();
         }));
 
-        it("makes a related where like clause", sinon.test(function (done) {
+        it("make a related where like clause", sinon.test(function (done) {
             var clause = JobFilter.makeWhereClause("problem.id", 1, "like");
             expect(clause).to.be.an.object;
             expect(clause.problem).to.be.an.object;
@@ -35,26 +37,32 @@ describe("job_filter module", function() {
         }));
     });
 
+        var data = {
+            jobgroups: [
+                {id: 1, name: "Group 1"},
+                {id: 2, name: "Group 2"},
+                {id: 5, name: "Group 3"},
+                {id: 8, name: "Group 4"},
+                {id: 12, name: "Group 5"},
+                {id: 20, name: "Group 6"},
 
-    var data = {
-        jobgroups: [
-            {id: 1, name: "Group 1"},
-            {id: 2, name: "Group 2"},
-            {id: 5, name: "Group 3"},
-            {id: 8, name: "Group 4"},
-            {id: 12, name: "Group 5"},
-            {id: 20, name: "Group 6"},
-
-        ],
-        jobs: [
-            {id: 101, name: "Job 1"},
-            {id: 102, name: "Job 2"},
-            {id: 105, name: "Job 3"},
-            {id: 108, name: "Job 4"},
-            {id: 112, name: "Job 5"},
-            {id: 120, name: "Job 6"},
-        ],
-    };
+            ],
+            jobs: [
+                {id: 101, name: "Job 1"},
+                {id: 102, name: "Job 2"},
+                {id: 105, name: "Job 3"},
+                {id: 108, name: "Job 4"},
+                {id: 112, name: "Job 5"},
+                {id: 120, name: "Job 6"},
+            ],
+            parameters: {
+                parameters: [{id: 1, name: "param1"},{id: 2, name: "param2"}],
+                valuesByName: {param1: ['value1', 'value2'], param2: ['value3', 'value4']},
+                parametersById: {1 : {id: 1, name: "param1"}, 2: {id: 2, name: "param2"}},
+                idsByName : {param1: 1, param2: 2},
+                namesById : {1: "param1", 2: "param2"}
+            }
+        };
     for (var i = 0; i < data.jobgroups.length; ++i) {
         data.jobgroups[i].definition = {model_name: "Jobgroup"};
         data.jobgroups[i].models_name = "Jobgroups";
@@ -64,6 +72,71 @@ describe("job_filter module", function() {
         data.jobs[i].models_name = "Jobs";
     }
 
+    describe("handle parameter filter Update", function() {
+        it("adds a single parameter filter", sinon.test(function (done) {
+            var userInput = [ 'p', { paramId: 1, values: [ '0', '1', '3' ] } ];
+            var userInputSaved = userInput.slice();
+            var filters = {};
+
+            JobFilter.updateFilters(filters, userInput, data);
+            debug(filters);
+
+            expect(filters.parameters).to.be.an.object;
+            expect(filters.parameters["1"]).to.be.an.object;
+            expect(filters.parameters["1"].name).to.be.equal("param1");
+            expect(filters.parameters["1"].values).to.deep.equal(userInputSaved[1].values);
+            done();
+
+        }));
+
+        it("overwrites a parameter filter", sinon.test(function (done) {
+            /* Create filter */
+            var userInput = [ 'p', { paramId: 1, values: [ '0', '1', '3' ] } ];
+            var userInputSaved = userInput.slice();
+            var filters = {};
+            JobFilter.updateFilters(filters, userInput, data);
+            expect(filters.parameters).to.be.an.object;
+            expect(filters.parameters["1"]).to.be.an.object;
+            expect(filters.parameters["1"].name).to.be.equal("param1");
+            expect(filters.parameters["1"].values).to.deep.equal(userInputSaved[1].values);
+
+            /* Overwrite filter */
+            var userInput = [ 'p', { paramId: 1, values: [ 'alpha', 'beta', 'gamma'] } ];
+            var userInputSaved = userInput.slice();
+            var filters = {};
+            JobFilter.updateFilters(filters, userInput, data);
+            expect(filters.parameters).to.be.an.object;
+            expect(filters.parameters["1"]).to.be.an.object;
+            expect(filters.parameters["1"].name).to.be.equal("param1");
+            expect(filters.parameters["1"].values).to.deep.equal(userInputSaved[1].values);
+            done();
+        }));
+
+        it("adds two parameter filters", sinon.test(function (done) {
+            var filters = {};
+            var userInputSaved = [];
+
+            var userInput = [ 'p', { paramId: 1, values: [ '0', '1', '3' ] } ];
+            userInputSaved.push(userInput.slice());
+            JobFilter.updateFilters(filters, userInput, data);
+
+            userInput = [ 'p', { paramId: 2, values: [ 'a','b' ] } ];
+            userInputSaved.push(userInput.slice());
+            JobFilter.updateFilters(filters, userInput, data);
+
+            for (var i = 0; i < 2; ++i) {
+                debug(i);
+                expect(filters.parameters).to.be.an.object;
+                expect(filters.parameters[i+1]).to.be.an.object;
+                expect(i+1).to.be.equal(userInputSaved[i][1].paramId);
+                expect(filters.parameters[i+1].name).to.be.equal(data.parameters.parametersById[userInputSaved[i][1].paramId].name );
+                expect(filters.parameters[i+1].values).to.deep.equal(userInputSaved[i][1].values);
+            }
+
+            done();
+
+        }));
+    });
 
     describe("updateFilters", function() {
         it("add selection filter to empty filter", sinon.test(function(done) {
@@ -138,7 +211,6 @@ describe("job_filter module", function() {
             userInput = ['c', 's', [false, true]];
             expect(filters.successes).to.be.an.object;
             JobFilter.updateFilters(filters, userInput, data);
-            debug(filters);
 
             expect(filters.successes.selection).to.be.an.array();
             expect(filters.successes.selection[0]).to.be.equal(null);
@@ -225,7 +297,6 @@ describe("job_filter module", function() {
             var filters = {};
             var userInput = ['c', 's', ['true']];
             JobFilter.updateFilters(filters, userInput, data);
-            debug(filters);
             var string = JobFilter.filtersToString(filters);
 
             expect(string).to.match(/Selected/i);
@@ -240,7 +311,6 @@ describe("job_filter module", function() {
             JobFilter.updateFilters(filters, userInput, data);
             userInput = ['s', 's', ['pending', 'submitted']];
             JobFilter.updateFilters(filters, userInput, data);
-            debug(filters);
             var string = JobFilter.filtersToString(filters);
 
             expect(string).to.match(/Selected/i);
