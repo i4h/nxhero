eeadc9af4dc5dd601120c9b47deebefce408bfcb
Ingmar Vierhaus <vierhaus@zib.de>
2017-01-11 11:26:56 +0100

    Add base_processor module with test

---
+++ test/base_processor.js
@@ -0,0 +1,30 @@
+var expect  = require("chai").expect;
+var Store = require('openrecord/lib/store');
+var store = new Store();
+var debug       = require('debug')('nxhero');
+var sinon = require('sinon');
+
+require("./__shared");
+var SharedHooks = require('./shared_hooks');
+
+var processor = require('../processors/processor');
+var BaseProcessor = require('../lib/base_processor');
+
+
+
+
+describe("Test of static processor methods", function() {
+
+
+    describe("getApplicableProcessors", function() {
+        BaseProcessor.registry = {
+            p1: {processor: {label: "Binary 1"}, binaryTypes: ['binary1']}
+        };
+        it("one of one applicable", sinon.test(function(done) {
+            expect(BaseProcessor.getApplicableProcessorLabels(['binary1'])).to.be.deep.equal({p1: "Binary 1"});
+            expect(BaseProcessor.getApplicableProcessorLabels(['binary2'])).to.be.deep.equal({});
+            done();
+        }));
+    });
+});
+
