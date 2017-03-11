90b86cec962d63b311180765e9a8f647d89d1418
Ingmar Vierhaus <vierhaus@zib.de>
2017-03-11 20:37:07 +0100

    implement duration parsing method

--- test/date.js
+++ test/date.js
@@ -23,5 +23,34 @@ describe("Test of date module", function() {
             done();
         }));
     });
+    describe("method parseDurationString", function() {
+
+        it("one-element input without unit", sinon.test(function(done) {
+            expect(date.parseDurationString("1")).to.be.equal(1);
+            done();
+        }));
+
+        it("one-element inputs", sinon.test(function(done) {
+            expect(date.parseDurationString("1s")).to.be.equal(1);
+            expect(date.parseDurationString("1m")).to.be.equal(60);
+            expect(date.parseDurationString("1h")).to.be.equal(3600);
+            done();
+        }));
+
+        it("two-element inputs without unit", sinon.test(function(done) {
+            expect(date.parseDurationString("1m 1")).to.be.equal(61);
+            expect(date.parseDurationString("1h 1")).to.be.equal(3601);
+            done();
+        }));
+
+        it("two-element inputs", sinon.test(function(done) {
+            expect(date.parseDurationString("1m 1s")).to.be.equal(61);
+            expect(date.parseDurationString("1h 1m")).to.be.equal(3660);
+            expect(date.parseDurationString("1h 1s")).to.be.equal(3601);
+            done();
+        }));
+
+    });
+
 });
 
